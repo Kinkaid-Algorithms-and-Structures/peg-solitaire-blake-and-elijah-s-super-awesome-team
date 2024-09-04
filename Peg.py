74cbcdc1060670a288151702df7e878a245bf39c
blakeD1 <blake.davidson@kinkaid.org>
2024-09-04 09:20:21 -0500

    .

---
+++ Peg.py
@@ -0,0 +1,19 @@
+class PegHole:
+    def __init__(self,place,filled,tag,row,col):
+        self.place=place
+        self.filled=filled
+        self.tag=tag
+        self.row=row
+        self.col=col
+        pass
+
+
+
+'''
+        Pegs holes to need to be an object with these values
+        Place:0-14
+        Filled:Boolean
+        Tag:1-4 as modeled
+        row: row of hole
+        col: column of peg
+    '''
